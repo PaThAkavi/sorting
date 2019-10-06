f70f9b01a03e8773d8a5e3e7eb9fbc18619b282e
Avaneesh Pathak <pathakavaneesh@gmail.com>
2019-10-06 16:06:52 +0530

    Using functional programming implementation of bubble sort

--- bubblesort.c
+++ bubblesort.c
@@ -1,23 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
-void bubbleSort(int arr[]);
+void bubbleSort(int *arr);
+void printArray();
+void scanArray();
+
+int n;
+int arr[5];
 
 int main(){
-    int arr[5] = [11,43,25,55,32];
+    printf("Enter the size of array : \n");
+    scanf("%d", &n);
+    scanArray();
+    printf("Array before sorting : \n");
+    printArray();
+    printf("Array after sorting : \n");
     bubbleSort(arr);
     return 0;
 }
 
-void bubbleSort(int arr[]){
-    int n = 5;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            if(arr[i] > arr[j]){
+void bubbleSort(int *arr){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
                 int temp;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
+    }
+    printArray();
+}
+
+void printArray(){
+    for(int i = 0; i <= n-1; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void scanArray(){
+    printf("Enter the elements of the array : \n");
+    for(int i = 0; i <= n-1; i++){
+        scanf("%d", &arr[i]);
     }
 }
